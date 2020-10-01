311d6a7ccaf36d4e77c527f1bcbd09a794d6d019
Wietze Helmantel <helmantel@pythonunited.com>
2020-10-01 15:24:21 +0200

    Got rid of bootstrap-vue because it does not yet work for Vue3

--- src/App.vue
+++ src/App.vue
@@ -43,13 +43,6 @@
 <script>
 import Props from "./components/Props.vue";
 import PicturePile from "./components/PicturePile.vue";
-import Vue from "vue";
-import BootstrapVue from "bootstrap-vue";
-
-Vue.use(BootstrapVue);
-
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 import store from "./store/index";
 
 export default {
